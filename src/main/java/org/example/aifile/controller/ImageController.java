3aa31bef17508b01397db8dc7d56988cc24be58a
jmin4078 <wpalsrl8588@gmail.com>
2026-08-10 11:51:33 +0900

    Add image explanation service

--- src/main/java/org/example/aifile/controller/ImageController.java
+++ src/main/java/org/example/aifile/controller/ImageController.java
@@ -1,6 +1,7 @@
 package org.example.aifile.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.aifile.service.ImageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,11 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 @Controller
 @RequestMapping("/image")
 @RequiredArgsConstructor
 public class ImageController {
+
+    private final ImageService imageService;
 
     @GetMapping
     public String formPage() {
@@ -26,6 +28,8 @@ public class ImageController {
         System.out.println(file.getContentType());
         System.out.println(file.getOriginalFilename());
         redirectAttributes.addFlashAttribute("msg", "파일 업로드 완료");
+        String answer = imageService.explain(file);
+        redirectAttributes.addFlashAttribute("answer", answer);
         return "redirect:/image";
     }
 }
